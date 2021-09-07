3a212784b00246549d6b5c245b81b3717f7f296d
khadkakrishna <krishnakhadka2802@gmail.com>
2021-09-07 22:37:13 +0530

    Spring Boot Application Iniatial Commit

---
+++ src/main/java/com/development/SpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/development/SpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
