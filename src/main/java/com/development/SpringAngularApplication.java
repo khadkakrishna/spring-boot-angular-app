cad8de7c8245afc949195ecfcb125e737b16e498
khadkakrishna <krishnakhadka2802@gmail.com>
2021-09-07 22:53:46 +0530

    Disabling login page of spring security

--- src/main/java/com/development/SpringAngularApplication.java
+++ src/main/java/com/development/SpringAngularApplication.java
@@ -3,7 +3,9 @@ package com.development;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class SpringAngularApplication {
 
 	public static void main(String[] args) {
